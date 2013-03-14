b791dff279a3af596e69ae0311c1dd91698d9cc9
Marc Bourguère <m.bourguere@gmail.com>
2013-03-14 15:49:35 +0100

    Server Done

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,25 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json.JsValue
+import play.api.libs.iteratee.Iteratee
+import play.api.libs.iteratee.Concurrent
 
-object Application extends Controller {
+object Application extends Controller{
   
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    def index = Action {
+	  Ok(views.html.index("Your new applAAAAication is ready."))
   }
+    
+    val (out, channelClient) = Concurrent.broadcast[JsValue]
+    
+    val in = Iteratee.foreach[JsValue] {E => channelClient.push(E)}
+    
+  def connect() = play.api.mvc.WebSocket.using[JsValue] { implicit request =>
+    
+    (in, out)
+  }
+
+  
   
 }
