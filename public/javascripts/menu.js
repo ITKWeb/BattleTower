5b0b430b8e0ece5d8f0267b0ec182549e48f4855
Romain MANESCHI <rmaneschi@ITK-P-201202-03.(none)>
2013-03-15 17:14:50 +0100

    test

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -15,38 +15,50 @@ var menu = (function() {
         var towers = [];
     
         addTowerDiv.onclick = function() {
-            if(isEditingMode == true) {
+            //if(isEditingMode == true) {
                 selectedElement = "Tower";
                 addTowerDiv.className = "btn select";
                 addWarriorDiv.className = "btn";
-            } else {
-                window.alert("Le jeu n'a pas commencé");
-            }
+            //} else {
+            //    window.alert("Le jeu n'a pas commencé");
+            //}
         }
         
         addWarriorDiv.onclick = function() {
-            if(isEditingMode == true) {
+            //if(isEditingMode == true) {
                 selectedElement = "Warrior";
                 addWarriorDiv.className = "btn select";
                 addTowerDiv.className = "btn";
-            } else {
-                window.alert("Le jeu n'a pas commencé");
+            //} else {
+            //    window.alert("Le jeu n'a pas commencé");
+            //}
+        }
+        
+        document.getElementById('btnDeadWarrior').onclick = function() {
+            for(var i=0; i<warriors.length; i++) {
+                board.setState(warriors[i],"Dead");
+            }
+        }
+        
+        document.getElementById('btnRunWarrior').onclick = function() {
+            for(var i=0; i<warriors.length; i++) {
+                board.setState(warriors[i],"Running");
             }
         }
         
         stage.onclick = function(evt) {
             console.log(isEditingMode, selectedElement);
-            if(isEditingMode == true && selectedElement) {
+            //if(isEditingMode == true && selectedElement) {
                 if(selectedElement == "Warrior") {
                     warriors.push(board.addItem(selectedElement, evt.clientX - 250, evt.clientY));
                 } else {
                     towers.push(board.addItem(selectedElement, evt.clientX - 250, evt.clientY));
                 }   
-            } else if(isEditingMode == false) {
-                window.alert("Le jeu n'a pas commencé");
-            } else {
-                window.alert("Selectionné un élément à ajouter");
-            }
+            //} else if(isEditingMode == false) {
+            //    window.alert("Le jeu n'a pas commencé");
+            //} else {
+            //    window.alert("Selectionné un élément à ajouter");
+            //}
         }
         
         network.addStartGameCallback(function(data) {
@@ -64,23 +76,27 @@ var menu = (function() {
                 isEditingMode = false;
                 addWarriorDiv.className = "btn";
                 addTowerDiv.className = "btn";
-                var board = [];
+                /*var board = [];
                 for(var i=0; i<warriors.length; i++) {
                     board.push(warriors[i]);
                 }
                 for(var i=0; i<towers.length; i++) {
                     board.push(towers[i]);
                 }
-                network.send({cmd:"endEditing", board:board});
+                network.send({cmd:"endEditing", board:board});*/
+                startGame({});
             },30000);
         }
         
         function startGame(data) {
             console.log(data);
-            for(var i=0; i<data.p1.length; i++) {
+            /*for(var i=0; i<data.p1.length; i++) {
                 board.setState(warriors[i],"Running");
             }
             for(var i=0; i<data.p2.length; i++) {
+                board.setState(warriors[i],"Running");
+            }*/
+            for(var i=0; i<warriors.length; i++) {
                 board.setState(warriors[i],"Running");
             }
         }
