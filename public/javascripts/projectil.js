6d1907c8238346cb2aadeac9d2480f9fe8e51d92
Romain MANESCHI <rmaneschi@ITK-P-201202-03.(none)>
2013-04-23 16:07:01 +0200

    direction of projectil

--- public/javascripts/projectil.js
+++ public/javascripts/projectil.js
@@ -1,10 +1,12 @@
 var Projectil = function(from, to) {
 
     var onHitCb = [];
-    var SIZE = {w:10, h:20, x:from.x, y:from.y};
+    var size = {w:5, h:5, x:from.x, y:from.y};
+    var speed = 2;
+    var oldDirection = { x: 0, y: 0 };
 
     function init() {
-        var e = Crafty.e("2D, Canvas, Collision, Color")
+        Crafty.e("2D, Canvas, Collision, Color")
             .attr(size)
             .color("red")
             .onHit("tower", function() {
@@ -12,10 +14,21 @@ var Projectil = function(from, to) {
                 for(var i=0; i<l; i++) {
                     onHitCb(from, to);
                 }
+                this.unbind("EnterFrame");
             })
             .collision()
             .bind("EnterFrame", function() {
-                craftyElem.move("e", 1.2);
+		        // Pixels to move are calculated from location and target every frame to handle the case when something else (IE, collision detection logic) changes our position.
+		        // Some cleaver optimization could probably eliminate the sqrt cost...
+		        var dx = to.x - this.x, dy = to.y - this.y,
+		        movX = (dx * this._speed) / (Math.sqrt(dx * dx + dy * dy)),
+		        movY = (dy * this._speed) / (Math.sqrt(dx * dx + dy * dy));
+
+		        oldDirection = { x: movX, y: movY };
+
+		        // Moved triggered twice to allow for better collision logic (like moving along diagonal walls)
+		        this.x += movX;
+		        this.y += movY;
             });
     }
     
