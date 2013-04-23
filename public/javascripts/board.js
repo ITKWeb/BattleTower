5d3ac49322872b0b9723c429e7514d6c8b8e7dc6
michele <mscapin@ITK-P-201202-04.(none)>
2013-04-23 17:00:37 +0200

    second forbiddenArea

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -7,27 +7,33 @@ var board = (function() {
 	var forbiddenAreas = [];
 
 
-	function isForbidden(item1,item2) {
+	function isForbidden(item1, x2,y2,w2,h2) {
 
-		var x1 = item1.craftyElem.x;
-		var w1 = item1.craftyElem.w;
+		var x1 = item1.x;
+		var y1 = item1.y;
 
-		var y1 = item1.craftyElem.y;
-		var h1 = item1.craftyElem.h;
+		if (item1.type == "Tower"){
+			var w1 = 40;
+			var h1 = 40;
+		}else{	//Warrior	
+			var w1 = 40;
+			var h1 = 40;
+		}
 
-		var x2 = item2.craftyElem.x;
-		var w2 = item2.craftyElem.w;
+//		console.log(x1);
+	//	console.log(x2);
+		//console.log(y1);
+//		console.log(y2);
 
-		var y2 = item2.craftyElem.y;
-		var h2 = item2.craftyElem.h;
-
-
-		if ( (x1 > x2 && x1< x2+w2) && (y1>y2 && y1<y2+h2) ) || 
-			 ( (x1+w1 > x2 && x1+w1<x2+w2) && (y1+h1> y2 && y1+h1<y2+w2) ) || 
-		   ( (x1+w1 > x2 && x1+w1<x2+w2) && (y1>y2 && y1<y2+h2) ) || 
-			 ( (x1>x2 && x1<x2+w2) && (y1+h1>y2 && y1+h1<y2+h2) ) {
+		if ( ( (x1 > x2 && x1< x2+w2) && (y1>y2 && y1<y2+h2) ) || 
+			   ( (x1+w1 > x2 && x1+w1<x2+w2) && (y1+h1> y2 && y1+h1<y2+w2) ) || 
+		     ( (x1+w1 > x2 && x1+w1<x2+w2) && (y1>y2 && y1<y2+h2) ) || 
+			   ( (x1>x2 && x1<x2+w2) && (y1+h1>y2 && y1+h1<y2+h2) ) 
+       ) {
+			console.log("isForbidden true");
 			return true;		
 		}
+			console.log("isForbidden false");
 		return false;
   }
 
@@ -46,16 +52,30 @@ var board = (function() {
 			return item;
 		},
 		
-		isForbiddenArea: function(item){
+		isForbiddenArea:function(item) {
+				console.log(items.length);
+				console.log(item.player);
+				// check for third part
 				
+				if (item.player === 1){
+					if (isForbidden(item, 200, 0, 400, 600 )) {
+							return true;
+					}
+				}else{
+					if (isForbidden(item, 0, 0, 400, 600 )) {
+							return true;
+					}
+				}
+
 				for(var i=0; i<items.length; i++) {
-       		  if (isForbidden(item, items[i])){
+						console.log(items[i].getItem().x);
+						console.log(items[i].getItem().y);
+       		  if (isForbidden(item, items[i].getItem().x, items[i].getItem().y, 40, 40 )) {
 							return true;
 						}
       	}
 			return false;
-			}
-		}
+		},
 		
 		//Fonction pour l'ajout d'un élément sur le plateau de jeu
 		addItem:function(item) {
